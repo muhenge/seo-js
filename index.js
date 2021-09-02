982d9b1033a13e9d1e91f42259037edd7197238e
henzy <mugunga5@gmail.com>
2021-09-02 16:16:25 +0200

    Set up meta elements in progress

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const baseUrl = "https://api-seo.cloudhost.cm";
 
   const apifetch = `${baseUrl}/api/seo/get-project`;
 
-
-
   const apiCall = async (data) => {
     let req = {
       method: "POST",
@@ -22,14 +20,26 @@ const baseUrl = "https://api-seo.cloudhost.cm";
       console.log(req.message);
     }
 
-    const test = document.querySelector('body');
-    const scr = document.querySelector('script');
-    console.log(scr);
+    const test = document.querySelector('body')
+    const scr = document.querySelector('script')
+    //console.log(scr)
     const attr = scr.getAttribute('src')
     console.log(attr)
-  scr.setAttribute('src', `${attr}{${jsonData.project.app_key}}`);
-    console.log(src)
-   
+    console.log(attr.includes(data.app_key))
+    if (attr.includes(data.app_key) === true) {
+      const desc = document.createElement('meta');
+      const keywords = document.createElement('meta');
+      const title = document.createElement('title');
+      const head = document.querySelector('head');
+      title.innerText = jsonData.project.meta_title;
+      keywords.setAttribute('name', 'keywords')
+      keywords.setAttribute('content', jsonData.project.meta_keywords)
+      desc.setAttribute('name', 'description')
+      desc.setAttribute('content', jsonData.project.meta_description)
+      head.appendChild(desc)
+      head.appendChild(keywords)
+      head.appendChild(title)
+    }
   }
 
   apiCall({
